fed7f0a538d0fd82f73a8aaef21a8ec650613ac7
yutaDV <74568169+yutaDV@users.noreply.github.com>
2024-06-18 20:40:00 +0000

    Deploying to gh-pages from @ yutaDV/goit-js-hw-10@73eb4d8639413a639ec92ca5ad2eb3ceb11943eb 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=parseInt(t.target.delay.value),o=t.target.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"✅",message:` Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌",message:` Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
