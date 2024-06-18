73eb4d8639413a639ec92ca5ad2eb3ceb11943eb
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-06-18 23:39:27 +0300

    promis

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delay = parseInt(event.target.delay.value);
+    const state = event.target.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+        .then(delay => {
+            iziToast.success({
+                title: '✅',
+                message: ` Fulfilled promise in ${delay}ms`,
+                position: 'topRight'
+            });
+        })
+        .catch(delay => {
+            iziToast.error({
+                title: '❌',
+                message: ` Rejected promise in ${delay}ms`,
+                position: 'topRight'
+            });
+        });
+});
+
